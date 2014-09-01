a77f29b817b934a4191bbfb92b0405088e01204c
Michael Schober <mschober@expedia.com>
2014-09-01 09:55:21 -0700

    working on reverse

--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -1,16 +1,74 @@
-class LinkedList:
-    def __init__(self, value=None, next=None):
+class Node:
+
+    def __init__(self, value, next=None):
         self.value = value
         self.next = next
-        self.list_length = 1 if value else 0
-
-    def length(self):
-        return self.list_length
 
     def __repr__(self):
-        repr_str = ""
-        repr_str += str(self.value) if self.value
+        return str(self.value) 
+
+class LinkedList:
+
+    def __init__(self, value=None):
+        self.head = None
+
+        if value:
+            self.add_before(value)
+
+    def add_before(self, value):
+        new_node = Node(value, self.head)
+        self.head = new_node
 
     def add_after(self, value):
-        self.next = LinkedList(value)
-        self.list_length += 1
+        if self.head:
+            self.add_after_recur(self.head, Node(value))
+        else:
+            self.add_before(value)
+
+    def add_after_recur(self, curr, node):
+        if curr.next:
+            return self.add_after_recur(curr.next, node)
+        else:
+            curr.next = node
+
+    def reverse(self):
+        reversed = LinkedList()
+        curr = self.head
+        while curr.next:
+            reversed.add_before(curr.value)
+        self.head = reversed.head
+
+    def reverse_bad(self):
+        if self.head:
+            self.reverse_recur(self.head)
+
+    def reverse_recur(self, curr):
+        #4 3 2 1
+        #1: 4.next = 3
+        #1: 
+        if curr.next:
+            self.reverse_recur(curr.next).next = curr
+            curr.next = None
+            ##
+            # Stack trace
+            trace = LinkedList()
+            trace.head = curr
+            print "stack: " + str(trace)
+        else:
+            return curr
+
+
+    def __repr__(self):
+        if self.head:
+            return self.construct_repr(self.head)
+        else:
+            return ""
+
+    def construct_repr(self, node):
+        def str_with_delim(val, delim=" "):
+            return str(val) + delim
+
+        if node.next:
+            return str_with_delim(node) + self.construct_repr(node.next)
+        else:
+            return str(node)
--- LinkedList/test_linked_list.py
+++ LinkedList/test_linked_list.py
@@ -2,28 +2,43 @@ import unittest
 from linked_list import *
 
 class TestLinkedList(unittest.TestCase):
+    '''This is the absolute vanilla linked list implementation
+       I can print the list
+       I can add a node to the front of the list
+       I can create a list
+    '''
 
     def test_empty_list(self):
         ll = LinkedList()
-        self.assertEqual(ll.length(), 0)
         self.assertEqual(str(ll), "")
-        self.assertNotEqual(ll, None)
 
     def test_single_node(self):
         ll = LinkedList(1)
-        self.assertEquals(ll.length(), 1)
-
-    def test_print_list(self):
-        ll = LinkedList(1)
         self.assertEquals(str(ll), "1")
 
-    def test_insert_node(self):
-        ll = LinkedList(1)
-        ll.add_after(2)
-        self.assertEquals(ll.length(), 2)
+    def test_add_before(self):
+        ll = LinkedList()
+        ll.add_before(4)
+        ll.add_before(3)
+        ll.add_before(2)
+        ll.add_before(1)
+        self.assertEquals(str(ll), "1 2 3 4")
 
-    #def test_print_list(self):
-    #    ll = LinkedList(1)
-    #    ll.insert(2)
-    #    self.assertEquals(str(ll), "1 2")
+    def test_add_after(self):
+        ll = LinkedList()
+        ll.add_after(1)
+        ll.add_after(2)
+        ll.add_after(3)
+        ll.add_after(4)
+        self.assertEquals(str(ll), "1 2 3 4")
+
+    def test_reverse(self):
+        ll = LinkedList()
+        ll.add_before(1)
+        ll.add_before(2)
+        ll.add_before(3)
+        ll.add_before(4)
+        ll.reverse()
+        self.assertEllqual(str(ll), "1 2 3 4")
+
 
