563570b727986d53598d1c77e349b9ba51b300e8
Michael Schober <mschober@expedia.com>
2014-09-01 07:32:24 -0700

    more tests"

--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -1,7 +1,16 @@
 class LinkedList:
-    def __init__(self, value, next=None):
+    def __init__(self, value=None, next=None):
         self.value = value
         self.next = next
+        self.list_length = 1 if value else 0
 
     def length(self):
-        return 1
+        return self.list_length
+
+    def __repr__(self):
+        repr_str = ""
+        repr_str += str(self.value) if self.value
+
+    def add_after(self, value):
+        self.next = LinkedList(value)
+        self.list_length += 1
--- LinkedList/test_linked_list.py
+++ LinkedList/test_linked_list.py
@@ -3,6 +3,27 @@ from linked_list import *
 
 class TestLinkedList(unittest.TestCase):
 
+    def test_empty_list(self):
+        ll = LinkedList()
+        self.assertEqual(ll.length(), 0)
+        self.assertEqual(str(ll), "")
+        self.assertNotEqual(ll, None)
+
     def test_single_node(self):
         ll = LinkedList(1)
         self.assertEquals(ll.length(), 1)
+
+    def test_print_list(self):
+        ll = LinkedList(1)
+        self.assertEquals(str(ll), "1")
+
+    def test_insert_node(self):
+        ll = LinkedList(1)
+        ll.add_after(2)
+        self.assertEquals(ll.length(), 2)
+
+    #def test_print_list(self):
+    #    ll = LinkedList(1)
+    #    ll.insert(2)
+    #    self.assertEquals(str(ll), "1 2")
+
