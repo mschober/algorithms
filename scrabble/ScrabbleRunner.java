ccdb9fd91f024cdf9a84678f34780ba4fbdde41c
Michael Schober <mschober@expedia.com>
2014-09-06 20:40:02 -0700

    scrabble latest

--- scrabble/ScrabbleRunner.java
+++ scrabble/ScrabbleRunner.java
@@ -1,67 +1,63 @@
-expedia.com
+import java.util.*;
 
-             e
-      p               x
-   e     d         i      a
- c   .            m  o
+public class ScrabbleRunner{
 
-[[lobster to],
-[ r        ]
-[ b        ]
-[          ]
-[ i        ]
-[ next     ]]
-
-
-int height = brd.length
-int width = brd[0].length
-
-words = []
-columns = [width]
-for(int r = 0; r < height; r++){
- row_wrd = ""
- for(int c = 0; c < width; c++){
-   if( brd[r][c] != ' '){
-     row_wrd += brd[r][c];
-     if (r == width-1 && row_wrd.length > 1) {
-        words.append(row_wrd);
-        row_wrd = "";
-    }
-     if (c == height-1 && columns[c].length > 1) {
-        words.append(columns[c]);
-        columns[c] = "";
-    }
-     if (columns[c] == null) {
-        columns[c] = brd[r][c]
-     } else {
-        columns[c] += brd[r][c]
-     }
-   } else {
-     if(row_wrd.length > 1){
-        words.append(row_wrd);
-        row_wrd = "";
-    }
-    if(columns[c].length > 1 {
-        words.append(columns[c]);
-        columns[c] = "";
+  public static void showBoard(char[][] board){
+    int height = board.length;
+    int width = board[0].length;
+    for(int r = 0; r < height; r++){
+      String rowline = "";
+      for(int c = 0; c < width; c++){
+        rowline += board[r][c];
+      }
+      System.out.println(rowline);
     }
 
-   }
-   
-   
- }
+  }
 
+  public static void main(String[] args){
+    List<String> words = new ArrayList<String>();
+
+    char[][] board = { "octopus top".toCharArray(),
+                       " a        2".toCharArray(),
+                       " totaled  3".toCharArray(),
+                       "       spit".toCharArray(),
+                       "         l ".toCharArray(),
+                       "         l ".toCharArray(),
+                     };
+    int height = board.length;
+    int width = board[0].length;
+
+    String[] columns = new String[width];
+    for(int r = 0; r < height; r++){
+      String row_wrd = "";
+      for(int c = 0; c < width; c++){
+        if( board[r][c] != ' '){
+          row_wrd += board[r][c];
+          if (r == width-1 && row_wrd.length() > 1) {
+             words.append(row_wrd);
+             row_wrd = "";
+          }
+          if (c == height-1 && columns[c].length() > 1) {
+             words.append(columns[c]);
+             columns[c] = "";
+          }
+          if (columns[c] == null) {
+             columns[c] = board[r][c] + "";
+          } else {
+             columns[c] += board[r][c];
+          }
+        } else {
+          if(row_wrd.length() > 1){
+             words.append(row_wrd);
+             row_wrd = "";
+          }
+          if(columns[c].length() > 1 ){
+              words.append(columns[c]);
+              columns[c] = "";
+          }
+        }
+      }
+    }
+  }
 }
-
-def gen_cols(brd):
-    for 
-
-cols = gen_cols(brd);
-
-for row in brd:
-    row_wrds = "".join(row).split(' ')
-for col in cols:
-    col_wrds = "".join(col).split(' ')
-
-
-
