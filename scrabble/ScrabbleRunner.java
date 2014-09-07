48beda7fd5a494a9ba02bd543a884a5d6ae2b563
Michael Schober <mschober@expedia.com>
2014-09-07 10:02:27 -0700

    almost there :(

--- scrabble/ScrabbleRunner.java
+++ scrabble/ScrabbleRunner.java
@@ -2,62 +2,113 @@ import java.util.*;
 
 public class ScrabbleRunner{
 
-  public static void showBoard(char[][] board){
-    int height = board.length;
-    int width = board[0].length;
-    for(int r = 0; r < height; r++){
-      String rowline = "";
-      for(int c = 0; c < width; c++){
-        rowline += board[r][c];
-      }
-      System.out.println(rowline);
-    }
-
-  }
-
-  public static void main(String[] args){
-    List<String> words = new ArrayList<String>();
-
-    char[][] board = { "octopus top".toCharArray(),
+    public static final char[][] BOARD = {
+                       "octopus top".toCharArray(),
                        " a        2".toCharArray(),
                        " totaled  3".toCharArray(),
                        "       spit".toCharArray(),
                        "         l ".toCharArray(),
                        "         l ".toCharArray(),
                      };
-    int height = board.length;
-    int width = board[0].length;
 
-    String[] columns = new String[width];
-    for(int r = 0; r < height; r++){
-      String row_wrd = "";
-      for(int c = 0; c < width; c++){
-        if( board[r][c] != ' '){
-          row_wrd += board[r][c];
-          if (r == width-1 && row_wrd.length() > 1) {
-             words.append(row_wrd);
-             row_wrd = "";
-          }
-          if (c == height-1 && columns[c].length() > 1) {
-             words.append(columns[c]);
-             columns[c] = "";
-          }
-          if (columns[c] == null) {
-             columns[c] = board[r][c] + "";
-          } else {
-             columns[c] += board[r][c];
-          }
+    public static final int HEIGHT = BOARD.length;
+    public static final int WIDTH = BOARD[0].length;
+
+  public static void showBoard(){
+    for(int r = 0; r < HEIGHT; r++){
+      String rowline = "";
+      for(int c = 0; c < WIDTH; c++){
+        rowline += BOARD[r][c];
+      }
+      System.out.println(rowline);
+    }
+  }
+
+
+  public static void showWords(List<String> words){
+    for (String s : words){
+      System.out.println(s);
+    }
+  }
+
+  public static void showColumns(String[] columns){
+    for (String s : columns){
+      System.out.println(s);
+    }
+  }
+
+  public static void addCharacter(int r, int c, List<String> words, String rowWord, String[] columns){
+    rowWord += BOARD[r][c]; //add the character to the row word
+    if (r == WIDTH-1 && rowWord.length() > 1) { //if this is the last char in row add word
+       words.add(rowWord);
+       rowWord = "";
+    }
+    if (columns[c] == null) { //if uninitialized initialize
+       columns[c] = BOARD[r][c] + "";
+    } else { //add the letter to the column
+       columns[c] += BOARD[r][c];
+    }
+    if (c == HEIGHT-1 && columns[c].length() > 1) { //if this is the last character in the column add word
+       words.add(columns[c]);
+       columns[c] = "";
+    }
+  }
+
+
+  public static void checkMutateStringInList(){
+    List<String> myList = new ArrayList<String>();
+    String myWord = "What up dawg!?";
+
+    myList.add(myWord);
+    System.out.println(myList.get(0));
+    myWord = "";
+    System.out.println(myList.get(0));
+  }
+
+  public static void addWord(String word ,List<String> words){
+    if(word != null){
+      if(word.length() > 1){ //add row word
+        words.add(word);
+      }
+      word = null;
+    }
+  }
+
+  public static void main(String[] args){
+    showBoard();
+
+    List<String> words = new ArrayList<String>();
+
+    String[] columns = new String[WIDTH+1];
+    for (int i = 0; i< columns.length; i++){
+      columns[i] = "";
+    }
+
+    for(int r = 0; r < HEIGHT; r++){
+      for(int c = 0; c < WIDTH; c++){
+        if( BOARD[r][c] != ' '){ //if we have a character
+          //addCharacter(r, c, words, rowWord, columns);
+    columns[WIDTH] += BOARD[r][c]; //add the character to the row word
+    //if (r == WIDTH-1 && rowWord.length() > 1) { //if this is the last char in row add word
+    //   words.add(rowWord);
+    //   rowWord = "";
+    //}
+    //if (columns[c] == null) { //if uninitialized initialize
+    //   columns[c] = BOARD[r][c] + "";
+    //} else { //add the letter to the column
+    //   columns[c] += BOARD[r][c];
+    //}
+    //if (c == HEIGHT-1 && columns[c].length() > 1) { //if this is the last character in the column add word
+    //   words.add(columns[c]);
+    //   columns[c] = "";
+    //}
         } else {
-          if(row_wrd.length() > 1){
-             words.append(row_wrd);
-             row_wrd = "";
-          }
-          if(columns[c].length() > 1 ){
-              words.append(columns[c]);
-              columns[c] = "";
-          }
+          addWord(columns[WIDTH], words);
+          //addWord(columns[c], words);
         }
       }
+      System.out.println(columns[WIDTH]);
     }
+    showWords(words);
   }
 }
